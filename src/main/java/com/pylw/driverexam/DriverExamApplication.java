4821186ac3207a1582086fdc374467a50a2a8c88
Tianying Peng <pty.me@qq.com>
2018-08-04 11:28:30 +0800

    创建及初始化项目

---
+++ src/main/java/com/pylw/driverexam/DriverExamApplication.java
@@ -0,0 +1,12 @@
+package com.pylw.driverexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverExamApplication.class, args);
+	}
+}

447ceb376bc1ea7eef898baabd49c6039f5ae4e3
YangQuan666 <yangquan0705@gmail.com>
2018-08-06 11:50:59 +0800

    实现了登录的后台代码验证

--- src/main/java/com/pylw/driverexam/user/mapper/UserMapper.java
+++ src/main/java/com/pylw/driverexam/user/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.pylw.driverexam.user.model.User;
@@ -13,8 +14,12 @@ public interface UserMapper {
 	@Select("select * from users")
 	List<User> findAllUsers();
 	
-	@Select("select * from users where phone = #{phone}")
-	User findUserbyPhone(String phone);
+	
+	@Select("select * from users where phone = #{phone} and password = #{password}")
+	List<User> findUserbyPhone(@Param("phone") String phone,@Param("password") String password);
+	
+	@Select("select * from users where email = #{email} and password = #{password}")
+	List<User> findUserbyEmail(@Param("email") String email,@Param("password") String password);
 	
 	@Insert("insert into users values(0,#{phone},#{email},#{password})")
 	void creat(User user);

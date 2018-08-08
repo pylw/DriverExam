25c2d8c9e2c0541946a3d2666835a1bdd51a9d00
YangQuan666 <yangquan0705@gmail.com>
2018-08-08 09:10:14 +0800

    添加缺失的类导入

--- src/main/java/com/pylw/driverexam/user/controller/UserController.java
+++ src/main/java/com/pylw/driverexam/user/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.pylw.driverexam.user.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pylw.driverexam.user.model.User;

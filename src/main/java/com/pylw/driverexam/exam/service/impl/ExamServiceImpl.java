f34934a62f0ee93eec2d3f40b74ec5242efe92ce
Tianying Peng <pty.me@qq.com>
2018-08-13 12:17:03 +0800

    修改ExamServiceImpl的问题

--- src/main/java/com/pylw/driverexam/exam/service/impl/ExamServiceImpl.java
+++ src/main/java/com/pylw/driverexam/exam/service/impl/ExamServiceImpl.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.pylw.driverexam.exam.mapper.ExamMapper;
 import com.pylw.driverexam.exam.model.Question;
 import com.pylw.driverexam.exam.model.SubjectInfo;
 import com.pylw.driverexam.exam.service.ExamService;
 
+@Service
 public class ExamServiceImpl implements ExamService {
 
 	@Autowired

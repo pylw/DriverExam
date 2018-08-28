9f4881df2e384b1cd04ee0506e161a3b64663c84
imperferct-zheng <31381020+imperferct-zheng@users.noreply.github.com>
2018-08-28 16:03:44 +0800

    管理员登陆模块

--- src/main/java/com/pylw/driverexam/admin/controller/AdminController.java
+++ src/main/java/com/pylw/driverexam/admin/controller/AdminController.java
@@ -1,10 +1,8 @@
 package com.pylw.driverexam.admin.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;

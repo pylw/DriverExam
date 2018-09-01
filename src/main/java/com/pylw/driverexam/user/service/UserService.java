f47173f3b48696048739afdac9aad336448b7196
imperferct-zheng <31381020+imperferct-zheng@users.noreply.github.com>
2018-09-01 08:55:12 +0800

    用户登陆修改

--- src/main/java/com/pylw/driverexam/user/service/UserService.java
+++ src/main/java/com/pylw/driverexam/user/service/UserService.java
@@ -124,49 +124,52 @@ public class UserService {
 	}
 	
 	public User userLogin(User user) {
-		UserInfo userInfo = userMapper.findByUserId(user.getUserId());
-		String lastLogin = userInfo.getLastLogin();
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date currentTime = new Date();
-		String dateString = formatter.format(currentTime);
-		if(lastLogin != null && userInfo.getContinuousLogin() != null){
-			Date l=null;
-			try {
-				l = formatter.parse(lastLogin);
-			} catch (ParseException e) {
-				e.printStackTrace();
+		User u = userMapper.userLogin(user);
+		if(u!=null) {
+			UserInfo userInfo = userMapper.findByUserId(u.getUserId());
+			String lastLogin = userInfo.getLastLogin();
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			Date currentTime = new Date();
+			String dateString = formatter.format(currentTime);
+			if(lastLogin != null && userInfo.getContinuousLogin() != null){
+				Date l=null;
+				try {
+					l = formatter.parse(lastLogin);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+		        Calendar cal1 = Calendar.getInstance();
+		        cal1.setTime(l);
+		        Calendar cal2 = Calendar.getInstance();
+		        cal2.setTime(currentTime);
+		        int day1= cal1.get(Calendar.DAY_OF_YEAR);
+		        int day2 = cal2.get(Calendar.DAY_OF_YEAR);
+		        int year1 = cal1.get(Calendar.YEAR);
+		        int year2 = cal2.get(Calendar.YEAR);
+		        int timeDistance = 0 ;
+		        if(year1 != year2)   //不同一年
+		        {
+		            for(int i = year1 ; i < year2 ; i ++)
+		            {
+		                if(i%4==0 && i%100!=0 || i%400==0)    //闰年            
+		                {
+		                    timeDistance += 366;
+		                }
+		                else    //不是闰年
+		                {
+		                    timeDistance += 365;
+		                }
+		            }
+		        }
+		        if(timeDistance + day2-day1==1) {
+		        	userInfo.setContinuousLogin(Integer.toString(Integer.parseInt(userInfo.getContinuousLogin())+1));
+		        }
+			}else {
+	        	userInfo.setContinuousLogin("1");
 			}
-	        Calendar cal1 = Calendar.getInstance();
-	        cal1.setTime(l);
-	        Calendar cal2 = Calendar.getInstance();
-	        cal2.setTime(currentTime);
-	        int day1= cal1.get(Calendar.DAY_OF_YEAR);
-	        int day2 = cal2.get(Calendar.DAY_OF_YEAR);
-	        int year1 = cal1.get(Calendar.YEAR);
-	        int year2 = cal2.get(Calendar.YEAR);
-	        int timeDistance = 0 ;
-	        if(year1 != year2)   //不同一年
-	        {
-	            for(int i = year1 ; i < year2 ; i ++)
-	            {
-	                if(i%4==0 && i%100!=0 || i%400==0)    //闰年            
-	                {
-	                    timeDistance += 366;
-	                }
-	                else    //不是闰年
-	                {
-	                    timeDistance += 365;
-	                }
-	            }
-	        }
-	        if(timeDistance + day2-day1==1) {
-	        	userInfo.setContinuousLogin(Integer.toString(Integer.parseInt(userInfo.getContinuousLogin())+1));
-	        }
-		}else {
-        	userInfo.setContinuousLogin("1");
+			userInfo.setLastLogin(dateString);
+			userMapper.updateUserInfo(userInfo);
 		}
-		userInfo.setLastLogin(dateString);
-		userMapper.updateUserInfo(userInfo);
 		return userMapper.userLogin(user);
 	}
 
